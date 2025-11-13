e77de298384540f013b66ada98b309e9e9ac1e2e
Sakis_Bog <thanbogiann@gmail.com>
2025-11-13 15:19:16 +0200

    Add expense form state and validation logic
    Introduced local state for amount, description, and category fields in AddExpense component. Added input validation, normalization, and handlers for form submission, ensuring all fields are completed and constraints are met before adding an expense.

--- src/components/MainPage/MainData/AddExpense.tsx
+++ src/components/MainPage/MainData/AddExpense.tsx
@@ -1,7 +1,73 @@
 
+import { useState } from "react";
 import CustomButton from "../../Buttons/CustomButton";
 
-const AddExpense = () => {
+interface AddExpenseProps {
+    onAddExpense: (
+        amount: string, 
+        description: string, 
+        category: string
+    ) => void;
+};
+
+const AddExpense = ({onAddExpense}: AddExpenseProps) => {
+    const [amount,setAmount]= useState('');
+    const [description, setDescription]= useState('');
+    const [category, setCategory] = useState('');
+
+    const handleAmountChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        let value = e.target.value;
+
+        value = value.replace(',', '.');
+        
+        const parts = value.split('.');
+        
+        // Αν έχει δεκαδικά (περισσότερα από 1 μέρη)
+        if (parts.length > 1) {
+            // Κόψε τα δεκαδικά σε 2 ψηφία
+            if (parts[1].length > 2) {
+                value = parts[0] + '.' + parts[1].substring(0, 2);
+            };
+        };
+        
+        setAmount(value);
+    };
+
+
+    const handleAddExpense = () =>{
+
+        if(
+            category.length===0 || 
+            description.length===0 ||
+            amount.length===0 
+        ) {
+            alert('You have to coplete all fields.')
+            return;
+        } else if(description.length>30){
+            alert('The description field must be up to 30 character.');
+            return;
+        } else if (category.length>20){
+            alert('The text of category must to have up to 20 characters.');
+            return;
+        } else {
+
+            // Κάνε normalize το amount (αν έχει κόμμα)
+            const normalizedAmount = amount.replace(',', '.');
+            const amountNumber = Number(normalizedAmount);
+            
+            if(amountNumber <= 0 || isNaN(amountNumber)){
+                alert('The amount must be grater than 0.00');
+                return;
+            };
+        };
+
+        onAddExpense(amount, description, category);
+        alert('Expense added to list !');
+        
+        setAmount('');
+        setDescription('');
+        setCategory('');
+    };
   return (
     <>
                 {/* Left div - Add Expenses */}
@@ -17,10 +83,12 @@ const AddExpense = () => {
                             Amount
                         </label>
                         <input
-                            type="text"
+                            type='text'
                             id="Amount"
+                            value={amount}
                             className="w-full border border-gray-300 rounded-lg p-2 mb-3"
                             placeholder="Enter the amount"
+                            onChange={handleAmountChange}
                         />
 
                         <label 
@@ -32,8 +100,10 @@ const AddExpense = () => {
                         <input
                             type="text"
                             id="Description"
+                            value={description}
                             className="w-full border border-gray-300 rounded-lg p-2 mb-3"
                             placeholder="Enter some words"
+                            onChange={(e)=> setDescription(e.target.value)}
                         />
 
                         <label 
@@ -45,14 +115,18 @@ const AddExpense = () => {
                         <input
                             type="text"
                             id="Category"
+                            value={category}
                             className="w-full border border-gray-300 rounded-lg p-2"
                             placeholder="Enter category"
+                            onChange={(e)=> setCategory(e.target.value)}
                         />
                     </div>
                     
                     <div className="flex justify-end mt-4">
                         <CustomButton  
                             color="blue"
+                            size='md'
+                            onClick={handleAddExpense}
                         >
                             + Add Expense
                         </CustomButton>
